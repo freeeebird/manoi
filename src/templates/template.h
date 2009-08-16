782c00834ff422cf61cb7d99ef2bebd505c432bd
rennhak <github@thewebagency.org>
2009-08-17 03:23:22 +0900

    - README fixup

---
+++ src/templates/template.h
@@ -0,0 +1,62 @@
+/*!
+ *
+ * \file        FILENAME
+ * \author      Bjoern Rennhak <bjoern@rennhak.de>
+ * \version     $Id$
+ * \brief       Header file
+ * \details     {
+ *                FIXME
+ *                FIXME
+ * }
+ *
+ * \example     ()                                                            // An example usage file
+ * \remarks     ()                                                            // Additional remarks about this file
+ * \namespace   ()                                                            // Description about the namespace of htis file
+ * \warning     ()                                                            // Warnings for this file
+ * \test        ()                                                            // Test cases for this file
+ * \note        {
+ *                Copyright (c) 2007, Bjoern Rennhak, Beat Tracking Project
+ *                All rights reserved, see COPYRIGHT file for more details.
+ *
+ *                o C code version used here is the ANSI C99 standard
+ *                o Code style used here is a modified Allman version
+ *                o Variable naming convention used here is a variation of the hungarian notation where appropriate
+ *                o Explicit shortform coding which the compiler also accepts has been avoided for the sake of clarity and unambiguity
+ *                o Documentation tool here used is DoxyGen ( http://www.doxygen.org ) which is licenced under GPLv2
+ *                    - Documenting style used here is the QT Style
+ *                    - http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ *                    - http://www.stack.nl/~dimitri/doxygen/commands.html
+ *                o A changelog can be found in the CHANGELOG file if appropriate
+ *                o Formatting is optimized for the VIM Text editor
+ *                    - configuration is done automatically by reading the details at the end of each file
+ *                    - tabs are converted to whitespaces
+ *                    - special folding is provided by explicit formatting tags
+ *                o Usage of Exuberant CTags, CScope, Lint and Valgrind is supported and encouraged
+ * }
+ *
+ */
+
+#ifndef _FILENAME_H_                                                 ///< Prevent recursive includes
+#define _FILENAME_H_                                                 ///< \def Define our current environment to prevent recursive includes
+
+// Standard header includes {{{
+#include "lib/copyright.h"                                           ///< Include our Copyright notice in every binary
+// }}}
+
+// Definitions {{{
+// }}}
+
+// Structure definitions {{{
+// }}}
+
+
+// Typedefs {{{
+// }}}
+
+// Functions {{{
+// }}}
+
+
+#endif // end of _FILENAME_H_
+
+// vim:ts=2:tw=100:wm=100
