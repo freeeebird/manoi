708b916fe47a62654e986c3d9b951876387949a8
rennhak <github@thewebagency.org>
2009-08-17 02:23:22 +0800

    - README fixup
    Signed-off-by: freeeebird <freebird@o2.pl>

---
+++ src/templates/template.c
@@ -0,0 +1,85 @@
+/*!
+ *
+ * \file        FILENAME.c
+ * \author      Bjoern Rennhak <bjoern@rennhak.de>
+ * \version     $Id$
+ * \brief       FIXME
+ * \details     {
+ *                FIXME
+ *                FIXME
+ * }
+ *
+ * \example     ()                                                            // An example usage file
+ * \remarks     ()                                                            // Additional remarks about this file
+ * \namespace   ()                                                            // Description about the namespace of htis file
+ * \warning     ()                                                            // Warnings for this file
+ * \test        ()                                                            // Test cases for this file
+ * \note        {
+ *                Copyright (c) 2007, Bjoern Rennhak, Beat Tracking Project
+ *                All rights reserved, see COPYRIGHT file for more details.
+ *
+ *                o C code version used here is the ANSI C99 standard plus GNU extensions (gnu9x)
+ *                o Code style used here is a modified Allman version
+ *                o Variable naming convention used here is a variation of the hungarian notation where appropriate
+ *                o Explicit shortform coding which the compiler also accepts has been avoided for the sake of clarity and unambiguity
+ *                o Documentation tool here used is DoxyGen ( http://www.doxygen.org ) which is licenced under GPLv2
+ *                    - Documenting style used here is the QT Style
+ *                    - http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ *                    - http://www.stack.nl/~dimitri/doxygen/commands.html
+ *                o A changelog can be found in the CHANGELOG file if appropriate
+ *                o Formatting is optimized for the VIM Text editor
+ *                    - configuration is done automatically by reading the details at the end of each file
+ *                    - tabs are converted to whitespaces
+ *                    - special folding is provided by explicit formatting tags
+ *                o Usage of Exuberant CTags, CScope, Lint and Valgrind is supported and encouraged
+ * }
+ *
+ */
+
+#define LOGGERID __FILE__                                                   ///< Logger ID in XML Configuration file
+
+
+// Standard Libary includes {{{
+#include <stdio.h>                                                            ///< For e.g. printf
+#include <stdlib.h>                                                           ///< For e.g. exit
+#include <string.h>                                                           ///< For e.g. String handling
+#include <unistd.h>                                                           ///< For e.g. parsing CMD Arguments
+#include <stdbool.h>                                                          ///< For e.g. true/false defines
+#include <assert.h>                                                           ///< For e.g. Unittesting, assertions
+
+
+// Custom Libary includes {{{
+#include "FILENAME.h"                                                              ///< Include the VFS Header file
+#include "bstrlib.h"                                                          ///< Include the BString Libary
+// }}}
+
+
+// Variable definitions {{{
+// }}}
+
+
+// {{{ Main function
+
+/*!
+ * \fn      int main( int argc, char* argv[] )
+ * \author  Bjoern Rennhak <bjoern@rennhak.de>
+ * \brief   
+ * \details {
+ *          }
+ *
+ * \param
+ * \param
+ * \return
+ *
+ * \todo
+ * \warning
+ */
+int main( int argc, char* argv[] )
+{
+}
+
+// {{{ Miscellaneous functions
+// }}}
+
+
+// vim:ts=2:tw=100:wm=100
